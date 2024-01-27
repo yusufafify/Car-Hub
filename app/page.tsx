1b9bb4d2f0bf7306aeaa86ae98233babf0e98eb4
yusufafify <youssef.mansour02@eng-st.cu.edu.eg>
2024-01-27 07:08:57 +0200

    added scroll event

--- app/page.tsx
+++ app/page.tsx
@@ -36,8 +36,10 @@ export default async function Home({ searchParams }: HomeProps) {
         {!isDataEmpty ? (
           <section>
             <div className='home__cars-wrapper'>
-              {allCars?.map((car) => (
-                <CarCard car={car} />
+              {allCars?.map((car,i) => (
+                <div key={i}>
+                  <CarCard car={car} />
+                </div>
               ))}
             </div>
 
