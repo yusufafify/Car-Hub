4c7e32cfd13267b7b80fa6bbdba349ac6ee79ca6
yusufafify <youssef.mansour02@eng-st.cu.edu.eg>
2024-01-27 06:54:02 +0200

    fixed typo

--- components/searchManufacturer/SearchManufacturer.tsx
+++ components/searchManufacturer/SearchManufacturer.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { SearchManufacturerProps } from "@/types";
+import { SearchManuFacturerProps } from "@/types";
 import React, { useState, Fragment } from "react";
 import { Combobox, Transition } from "@headlessui/react";
 import Image from "next/image";
@@ -7,8 +7,8 @@ import { manufacturers } from "@/constants";
 
 const SearchManufacturer = ({
   manufacturer,
-  setManufacturer,
-}: SearchManufacturerProps) => {
+  setManuFacturer,
+}: SearchManuFacturerProps) => {
   const [query, setQuery] = useState("");
   const filteredManufacturers =
     query === ""
@@ -21,7 +21,7 @@ const SearchManufacturer = ({
         );
   return (
     <div className="search-manufacturer">
-      <Combobox value={manufacturer} onChange={setManufacturer}>
+      <Combobox value={manufacturer} onChange={setManuFacturer}>
         <div className="relative w-full">
           <Combobox.Button className={"absolute top-[14px]"}>
             <Image
