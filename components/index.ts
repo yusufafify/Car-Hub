5c70e773ac526edfb36a1cde683af7346f55db9c
yusufafify <youssef.mansour02@eng-st.cu.edu.eg>
2024-01-27 06:49:52 +0200

    first commit

---
+++ components/index.ts
@@ -0,0 +1,12 @@
+import Hero from "./hero/Hero";
+import CustomButton from "./customButton/CustomButton";
+import Navbar from "./navbar/Navbar";
+import Footer from "./footer/Footer";
+import SearchBar from "./searchBar/SearchBar";
+import CustomFilter from "./customFilter/CustomFilter";
+import SearchManufacturer from "./searchManufacturer/SearchManufacturer";
+import CarCard from "./carCard/CarCard";
+import CarDetails from "./carDetails/CarDetails";
+import ShowMore from "./showmore/ShowMore";
+
+export { Hero, CustomButton,Navbar,Footer,SearchBar,CustomFilter,SearchManufacturer,CarCard,CarDetails,ShowMore };
